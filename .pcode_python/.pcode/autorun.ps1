9515ecdc15f163bf98f7b97aab2e5d2b5e38f864
Philip Hoffman <82.phil@gmail.com>
2018-07-08 11:22:04 -0700

    python script now activates virtualenv when starting terminal in workspace

--- .pcode_python/.pcode/autorun.ps1
+++ .pcode_python/.pcode/autorun.ps1
@@ -8,5 +8,17 @@ function code_alias{
     Write-Output "Python workspace alias $alias_name added"
 }
 
+# Workspace Aliases
+
 code_alias -alias_name "build" -script "build_env.ps1"
 code_alias -alias_name "clean" -script "clean_env.ps1"
+
+# Virtualenv Activate
+& {
+    $project_dir = if (Test-Path env:PWD) {$env:PWD} else {Get-Location}
+    $virtualenv_script = [io.path]::Combine(
+        $project_dir, "venv", "Scripts", "Activate.ps1")
+    if ([System.IO.File]::Exists($virtualenv_script)) {
+        & $virtualenv_script
+    }
+}
