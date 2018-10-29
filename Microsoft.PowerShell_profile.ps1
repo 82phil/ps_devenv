43f56d989efc143467e05b22d4ad4cc0ec226f07
Philip Hoffman <82.phil@gmail.com>
2018-10-28 21:45:24 -0700

    Adding gif to docs for demonstration
    Added signatures to powershell scripts

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,7 +2,6 @@
 $profile_dir = Split-Path -path $profile
 $python_init = [io.path]::Combine($profile_dir, ".pcode_python", ".pcode", ".init.ps1")
 New-Alias code_python $python_init
-Clear-Host
 
 # .pcode autorun script if available
 $project_dir = if (Test-Path env:PWD) {$env:PWD} else {Get-Location}
@@ -10,3 +9,42 @@ $pcode_autorun = [io.path]::Combine($project_dir, ".pcode", "autorun.ps1")
 if ([System.IO.File]::Exists($pcode_autorun)) {
     . $pcode_autorun
 }
+
+# SIG # Begin signature block
+# MIIGlwYJKoZIhvcNAQcCoIIGiDCCBoQCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUU7Sk5j8oxLnpC5os4eTFabIq
+# fQGgggPOMIIDyjCCArKgAwIBAgIQTP3uDUHglaREeCKNEB56ujANBgkqhkiG9w0B
+# AQUFADB9MSwwKgYDVQQLDCNodHRwczovL2dpdGh1Yi5jb20vODJwaGlsL3BzX2Rl
+# dmVudjEgMB4GCSqGSIb3DQEJARYRODIucGhpbEBnbWFpbC5jb20xFzAVBgNVBAoM
+# DlBoaWxpcCBIb2ZmbWFuMRIwEAYDVQQDDAlwc19kZXZlbnYwHhcNMTgxMDI5MDQy
+# OTM5WhcNMjExMDI5MDQzOTM5WjB9MSwwKgYDVQQLDCNodHRwczovL2dpdGh1Yi5j
+# b20vODJwaGlsL3BzX2RldmVudjEgMB4GCSqGSIb3DQEJARYRODIucGhpbEBnbWFp
+# bC5jb20xFzAVBgNVBAoMDlBoaWxpcCBIb2ZmbWFuMRIwEAYDVQQDDAlwc19kZXZl
+# bnYwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDBe3XlB9fSC7zg8tPI
+# vwsZumH2GgGVvz68Z1KSMLTXsny9bSgBo/K3B2r3j4V76u7SxLxkX4qMn1C5siQ5
+# 5CLytUxOx+DKaQuynDonlmR9XV+EkEnwSYoxQK3wCHqlTvX2fuYyzCl8YppT5Ao6
+# BfI8aRCHqGIjla2fu/pUR+MLroz10ikG0HPYxDyT29MOwh5UgIhtVG1lLVdtwhzk
+# OqaiKCZBz9qsAZ4cCHJcS0BJ0NxsEsmvvWsgiBY9DljEn2RDByshqJz+TZJ6pakX
+# iVTkJI2HtkkApL/DQSAfcF2JHgipspGP3129eTdyBBy9rnHG3qDxNWCJozoM7ky8
+# DwzxAgMBAAGjRjBEMA4GA1UdDwEB/wQEAwIHgDATBgNVHSUEDDAKBggrBgEFBQcD
+# AzAdBgNVHQ4EFgQUHtGBHiQ2iwZKRlOw+jyEmKsLiwEwDQYJKoZIhvcNAQEFBQAD
+# ggEBAKj+aoAfiysWmdnbuEIeF7aEG7L7/enfB8kzxIfgTKVWlXOneD+lY/jRbwfa
+# o/q30pWY7x+t6k13kacqNbgOIS09ni8i6ZTdgkzjErwfxRKU/iReGAUvDeg2ixFx
+# p1GIn/Kh3x8JagPoWAqwIwW2hdEHZgupP42UBC8zFG+FYIhAx/s/4YBLHJFnkgHC
+# gc9quDMlKsmLZXMaL01z/5PdwskHPjk+p8Q6GMrWQcO2xbHdlxMvCIZ/UaA5CCVE
+# lQ6fIOG94HQQtKJTK1YfQksbYM/J7Ix3cFlYx13JLXAz6kH2/R6phh0YRuEwpSaF
+# rqeFP4EjhKEOw/cJjhT4zO8jXpUxggIzMIICLwIBATCBkTB9MSwwKgYDVQQLDCNo
+# dHRwczovL2dpdGh1Yi5jb20vODJwaGlsL3BzX2RldmVudjEgMB4GCSqGSIb3DQEJ
+# ARYRODIucGhpbEBnbWFpbC5jb20xFzAVBgNVBAoMDlBoaWxpcCBIb2ZmbWFuMRIw
+# EAYDVQQDDAlwc19kZXZlbnYCEEz97g1B4JWkRHgijRAeerowCQYFKw4DAhoFAKB4
+# MBgGCisGAQQBgjcCAQwxCjAIoAKAAKECgAAwGQYJKoZIhvcNAQkDMQwGCisGAQQB
+# gjcCAQQwHAYKKwYBBAGCNwIBCzEOMAwGCisGAQQBgjcCARUwIwYJKoZIhvcNAQkE
+# MRYEFPyerxiPj8vkZevQWg1goHjpN323MA0GCSqGSIb3DQEBAQUABIIBAKT1gvvK
+# IYx8c9wQ0g6GKoTvn/yh2q+ULL4XGQb/Y+AMvOZa9pGt7MDFzCW7ZximhM/LsUak
+# 8rmIqiXKRkE7hw7XwNsCFDKI6X4BMLPBPnsXFBEb6v9tMhM/JTzRRt1EEMCnN0HZ
+# 02hnjt7Esv2H9iI+pQ4kc8J4CsorLNixNppdSAKJJjbJj+qAM7U06RO7sU8y6eui
+# AsjDJFqOjTY3qKXzDwIxx5spvTm+L10BIkOY+Q20SryXJZhsT6N2YDke+RN75fNS
+# qZsNsVfNFbuWCvg8ukyuV/qda9IyVcn2/oHZxqvtxumJ0iSbVviVT0/CBDDpRRhO
+# QCPGcEEkHyiqTgU=
+# SIG # End signature block
