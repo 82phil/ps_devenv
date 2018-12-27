a35c85143e615f89a7674093ed4abd14b81a6fb7
Philip Hoffman <82.phil@gmail.com>
2018-12-26 19:12:25 -0800

    Removed signing, fixing things

--- .pcode_python/.pcode/build_env.ps1
+++ .pcode_python/.pcode/build_env.ps1
@@ -83,7 +83,7 @@ if ($python_cores.Count -lt 1) {
     throw "Could not find a Python Installation!"
     exit 1
 }
-if ($python_cores.contains($pythonVersion)) {
+if (($pythonVersion) -and ($python_cores.contains($pythonVersion))) {
     $python_cores = @{$pythonVersion=$python_cores[$pythonVersion]}
 }
 if ($python_cores.count -gt 1) {
@@ -122,42 +122,3 @@ try {
 Write-Output "Installing PIP packages listed in requirements"
 & pip install -r .\requirements.txt
 Write-Output "Virtual Env up"
-
-# SIG # Begin signature block
-# MIIGlwYJKoZIhvcNAQcCoIIGiDCCBoQCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
-# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUAoqvFZ5UI70tFHJIQAkP1wt6
-# CvKgggPOMIIDyjCCArKgAwIBAgIQTP3uDUHglaREeCKNEB56ujANBgkqhkiG9w0B
-# AQUFADB9MSwwKgYDVQQLDCNodHRwczovL2dpdGh1Yi5jb20vODJwaGlsL3BzX2Rl
-# dmVudjEgMB4GCSqGSIb3DQEJARYRODIucGhpbEBnbWFpbC5jb20xFzAVBgNVBAoM
-# DlBoaWxpcCBIb2ZmbWFuMRIwEAYDVQQDDAlwc19kZXZlbnYwHhcNMTgxMDI5MDQy
-# OTM5WhcNMjExMDI5MDQzOTM5WjB9MSwwKgYDVQQLDCNodHRwczovL2dpdGh1Yi5j
-# b20vODJwaGlsL3BzX2RldmVudjEgMB4GCSqGSIb3DQEJARYRODIucGhpbEBnbWFp
-# bC5jb20xFzAVBgNVBAoMDlBoaWxpcCBIb2ZmbWFuMRIwEAYDVQQDDAlwc19kZXZl
-# bnYwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDBe3XlB9fSC7zg8tPI
-# vwsZumH2GgGVvz68Z1KSMLTXsny9bSgBo/K3B2r3j4V76u7SxLxkX4qMn1C5siQ5
-# 5CLytUxOx+DKaQuynDonlmR9XV+EkEnwSYoxQK3wCHqlTvX2fuYyzCl8YppT5Ao6
-# BfI8aRCHqGIjla2fu/pUR+MLroz10ikG0HPYxDyT29MOwh5UgIhtVG1lLVdtwhzk
-# OqaiKCZBz9qsAZ4cCHJcS0BJ0NxsEsmvvWsgiBY9DljEn2RDByshqJz+TZJ6pakX
-# iVTkJI2HtkkApL/DQSAfcF2JHgipspGP3129eTdyBBy9rnHG3qDxNWCJozoM7ky8
-# DwzxAgMBAAGjRjBEMA4GA1UdDwEB/wQEAwIHgDATBgNVHSUEDDAKBggrBgEFBQcD
-# AzAdBgNVHQ4EFgQUHtGBHiQ2iwZKRlOw+jyEmKsLiwEwDQYJKoZIhvcNAQEFBQAD
-# ggEBAKj+aoAfiysWmdnbuEIeF7aEG7L7/enfB8kzxIfgTKVWlXOneD+lY/jRbwfa
-# o/q30pWY7x+t6k13kacqNbgOIS09ni8i6ZTdgkzjErwfxRKU/iReGAUvDeg2ixFx
-# p1GIn/Kh3x8JagPoWAqwIwW2hdEHZgupP42UBC8zFG+FYIhAx/s/4YBLHJFnkgHC
-# gc9quDMlKsmLZXMaL01z/5PdwskHPjk+p8Q6GMrWQcO2xbHdlxMvCIZ/UaA5CCVE
-# lQ6fIOG94HQQtKJTK1YfQksbYM/J7Ix3cFlYx13JLXAz6kH2/R6phh0YRuEwpSaF
-# rqeFP4EjhKEOw/cJjhT4zO8jXpUxggIzMIICLwIBATCBkTB9MSwwKgYDVQQLDCNo
-# dHRwczovL2dpdGh1Yi5jb20vODJwaGlsL3BzX2RldmVudjEgMB4GCSqGSIb3DQEJ
-# ARYRODIucGhpbEBnbWFpbC5jb20xFzAVBgNVBAoMDlBoaWxpcCBIb2ZmbWFuMRIw
-# EAYDVQQDDAlwc19kZXZlbnYCEEz97g1B4JWkRHgijRAeerowCQYFKw4DAhoFAKB4
-# MBgGCisGAQQBgjcCAQwxCjAIoAKAAKECgAAwGQYJKoZIhvcNAQkDMQwGCisGAQQB
-# gjcCAQQwHAYKKwYBBAGCNwIBCzEOMAwGCisGAQQBgjcCARUwIwYJKoZIhvcNAQkE
-# MRYEFAX1jJsM7jWBQiIj3Wcx4yj6FpKlMA0GCSqGSIb3DQEBAQUABIIBAG5uw/YH
-# dXGaAVqRK28WpLKVlOR/ranDWihs6NKSElDbiX9mKHHxjM0WzhmLUzp6L/nz5c5L
-# vlTIXXIkgsFe9TWpGmgCKw03aS6f0SkH+kknZ9ulAihQpKG3oeM55/g4tprh8p4O
-# gFvo/H59jhiKydZm5z6rHtnA2dJ1mkSVVLRZQqwyOraRZ7moo+Qti1VSpL+XtTSX
-# bFrmpiPvwXeVkatkQ7CtQRpFOgcOi9G6qUVOwMaSxyPSlGzzxgjW2M72dXAWg96y
-# xc3gDpyZ0vJbKzOOY2OeK9OwbiB9zoqdgaoXGb5LUwFujCc5tvOcMOccqTvV4Jug
-# EMghWvlda4lXS0Y=
-# SIG # End signature block
--- ps_devenv.psm1
+++ ps_devenv.psm1
@@ -1,10 +1,12 @@
 function New-Code {
 param(
-    [string[]] $code_type
+    [Parameter(Mandatory=$true)][string]$code_type,
+    [Parameter(Mandatory=$false)][string]$option
 )
+    $code_type = $code_type.ToLower()
     if ($code_type -Eq "Python") {
         $python_init = [io.path]::Combine($PSScriptRoot, ".pcode_python", ".pcode", ".init.ps1")
-        . $python_init
+        . $python_init $option
     }
 }
 
@@ -16,44 +18,3 @@ function Enter-Code {
         . $pcode_autorun
     }
 }
-
-
-
-# SIG # Begin signature block
-# MIIGlwYJKoZIhvcNAQcCoIIGiDCCBoQCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
-# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUHlIJAJe9BH6XkBTGR7LwySbo
-# ud2gggPOMIIDyjCCArKgAwIBAgIQTP3uDUHglaREeCKNEB56ujANBgkqhkiG9w0B
-# AQUFADB9MSwwKgYDVQQLDCNodHRwczovL2dpdGh1Yi5jb20vODJwaGlsL3BzX2Rl
-# dmVudjEgMB4GCSqGSIb3DQEJARYRODIucGhpbEBnbWFpbC5jb20xFzAVBgNVBAoM
-# DlBoaWxpcCBIb2ZmbWFuMRIwEAYDVQQDDAlwc19kZXZlbnYwHhcNMTgxMDI5MDQy
-# OTM5WhcNMjExMDI5MDQzOTM5WjB9MSwwKgYDVQQLDCNodHRwczovL2dpdGh1Yi5j
-# b20vODJwaGlsL3BzX2RldmVudjEgMB4GCSqGSIb3DQEJARYRODIucGhpbEBnbWFp
-# bC5jb20xFzAVBgNVBAoMDlBoaWxpcCBIb2ZmbWFuMRIwEAYDVQQDDAlwc19kZXZl
-# bnYwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDBe3XlB9fSC7zg8tPI
-# vwsZumH2GgGVvz68Z1KSMLTXsny9bSgBo/K3B2r3j4V76u7SxLxkX4qMn1C5siQ5
-# 5CLytUxOx+DKaQuynDonlmR9XV+EkEnwSYoxQK3wCHqlTvX2fuYyzCl8YppT5Ao6
-# BfI8aRCHqGIjla2fu/pUR+MLroz10ikG0HPYxDyT29MOwh5UgIhtVG1lLVdtwhzk
-# OqaiKCZBz9qsAZ4cCHJcS0BJ0NxsEsmvvWsgiBY9DljEn2RDByshqJz+TZJ6pakX
-# iVTkJI2HtkkApL/DQSAfcF2JHgipspGP3129eTdyBBy9rnHG3qDxNWCJozoM7ky8
-# DwzxAgMBAAGjRjBEMA4GA1UdDwEB/wQEAwIHgDATBgNVHSUEDDAKBggrBgEFBQcD
-# AzAdBgNVHQ4EFgQUHtGBHiQ2iwZKRlOw+jyEmKsLiwEwDQYJKoZIhvcNAQEFBQAD
-# ggEBAKj+aoAfiysWmdnbuEIeF7aEG7L7/enfB8kzxIfgTKVWlXOneD+lY/jRbwfa
-# o/q30pWY7x+t6k13kacqNbgOIS09ni8i6ZTdgkzjErwfxRKU/iReGAUvDeg2ixFx
-# p1GIn/Kh3x8JagPoWAqwIwW2hdEHZgupP42UBC8zFG+FYIhAx/s/4YBLHJFnkgHC
-# gc9quDMlKsmLZXMaL01z/5PdwskHPjk+p8Q6GMrWQcO2xbHdlxMvCIZ/UaA5CCVE
-# lQ6fIOG94HQQtKJTK1YfQksbYM/J7Ix3cFlYx13JLXAz6kH2/R6phh0YRuEwpSaF
-# rqeFP4EjhKEOw/cJjhT4zO8jXpUxggIzMIICLwIBATCBkTB9MSwwKgYDVQQLDCNo
-# dHRwczovL2dpdGh1Yi5jb20vODJwaGlsL3BzX2RldmVudjEgMB4GCSqGSIb3DQEJ
-# ARYRODIucGhpbEBnbWFpbC5jb20xFzAVBgNVBAoMDlBoaWxpcCBIb2ZmbWFuMRIw
-# EAYDVQQDDAlwc19kZXZlbnYCEEz97g1B4JWkRHgijRAeerowCQYFKw4DAhoFAKB4
-# MBgGCisGAQQBgjcCAQwxCjAIoAKAAKECgAAwGQYJKoZIhvcNAQkDMQwGCisGAQQB
-# gjcCAQQwHAYKKwYBBAGCNwIBCzEOMAwGCisGAQQBgjcCARUwIwYJKoZIhvcNAQkE
-# MRYEFE1nkBd61LtfF2QXFMUBH7ZpE+TAMA0GCSqGSIb3DQEBAQUABIIBAAel+akL
-# j72XD/iUtk22i80YM6BpozQZQDgwc4YEXEl5EALAWt978FR+uJ1Y9gy//JN6QOfI
-# btoHZ9cj5/fOLz7hTBaRqTcLKYYIzeT+hz+mq3IWU+cK5a1VAuo1Ng7EZHSSQAoj
-# IC/0Xwi312T6GKIyxRZPp/ROgFXjQKMMJdEt62HNvZqJK1D9k28vDjpQ2/Qwo5Wy
-# 5SZbUuzsXASU9sb5RNaKc1jRtYcKkiLrAAqPvqD83bg9EXj2N+tYUdJlQRhu19GI
-# E7e7sdyd2L69iEs3dJjDCIpgHYcs3f3LtTatbtfoWjAHFaNhyAGvC7P4pgFPBH50
-# 5fUXegYWEVGFfsw=
-# SIG # End signature block
