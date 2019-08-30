944bd6f59c18bab4bf81545cfdf7401654ecdf40
Philip Hoffman <philip.hoffman@metergroup.com>
2019-08-30 06:44:55 -0700

    Attempting to address renaming

---
+++ DevEnv/.pcode_python/.pcode/.init.ps1
@@ -0,0 +1,7 @@
+$pcode_python = Join-Path -Path $PSScriptRoot ..
+
+# Copy stuff over...
+Write-Output "Copying Workspace..."
+Copy-Item -Path $pcode_python\* -Destination . -Recurse -Force
+# Build Environment
+& .pcode\build_env.ps1 $args[0]
