da33539c5c23c05eebc144eb9d8e25b4ee3d0e9a
Philip Hoffman <philip.hoffman@metergroup.com>
2019-08-30 06:46:09 -0700

    Hopefully these removed the ones under the old naming

--- devenv/.pcode_python/.pcode/info.py
+++
@@ -1,8 +0,0 @@
-import json
-import sys
-
-obj = {}
-obj["versionInfo"] = sys.version_info[:4]
-obj["is64Bit"] = sys.maxsize > 2**32
-
-print(json.dumps(obj))
--- devenv/.pcode_python/.pcode/pylint.ps1
+++
@@ -1,8 +0,0 @@
-$proc = (Get-CimInstance Win32_ComputerSystem).NumberOfLogicalProcessors
-if ($env::PYTHONPATH){
-    if (Test-Path -path (Join-Path $env::PYTHONPATH -ChildPath ".pylintrc")) {
-        &pylint -j"$proc" -f colorized --rcfile $env:PYTHONPATH/.pylintrc @(Get-ChildItem *.py)
-    }
-} else {
-    &pylint -j"$proc" -f colorized @(Get-ChildItem *.py)
-}
