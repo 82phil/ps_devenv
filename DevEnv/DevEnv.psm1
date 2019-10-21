017788cb0fb4a6f0750cc9b77d8b3e8a11c51797
BuildTools <unconfigured@null.spigotmc.org>
2019-10-21 12:50:04 -0700

    References .pcode folders for dev names

--- DevEnv/DevEnv.psm1
+++ DevEnv/DevEnv.psm1
@@ -4,9 +4,19 @@ param(
     [Parameter(Mandatory=$false)][string]$option
 )
     $code_type = $code_type.ToLower()
-    if ($code_type -Eq "Python") {
-        $python_init = [io.path]::Combine($PSScriptRoot, ".pcode_python", ".pcode", ".init.ps1")
-        . $python_init $option
+    $pcode_dirs = (Get-ChildItem -Path $PSScriptRoot -Directory
+        ).Name -like ".pcode_*"
+    $match = $pcode_dirs | Where-Object { $_ -Match ".pcode_$($code_type)" }
+    if ($match.count -lt 1) {
+        throw "Could not find a match for {0}\nThese options are available:\n{1)" -f ($code_type, $pcode_dirs -replace ".pcode_")
+    }
+    if ($match.count -gt 1) {
+        throw "Found too many matches\nPerhaps one of these:\n{0}" -f $match -replace ".pcode_"
+    }
+    $init_script = [io.path]::Combine(
+        $PSScriptRoot, $match, ".pcode", ".init.ps1")
+    if (Test-Path $init_script) {
+        . $init_script $option
     }
 }
 
