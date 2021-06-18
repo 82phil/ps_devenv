f639fbc0a1c0e72ef30cae2839d2c1d2a05a7c47
Philip Hoffman <82.phil@gmail.com>
2021-06-17 23:16:31 -0700

    Renamed default branch to main

--- deploy.psdeploy.ps1
+++ deploy.psdeploy.ps1
@@ -17,7 +17,7 @@
 if(
     $env:BHPSModulePath -and
     $env:BHBuildSystem -ne 'Unknown' -and
-    $env:BHBranchName -eq "master" -and
+    $env:BHBranchName -eq "main" -and
     $env:BHCommitMessage -match '!deploy'
 )
 {
@@ -35,7 +35,7 @@ else
 {
     "Skipping deployment: To deploy, ensure that...`n" +
     "`t* You are in a known build system (Current: $ENV:BHBuildSystem)`n" +
-    "`t* You are committing to the master branch (Current: $ENV:BHBranchName) `n" +
+    "`t* You are committing to the main branch (Current: $ENV:BHBranchName) `n" +
     "`t* Your commit message includes !deploy (Current: $ENV:BHCommitMessage)" |
         Write-Host
 }
